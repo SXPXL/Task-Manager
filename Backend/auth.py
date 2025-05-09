69f0acd2079b266d863a9700fd10f9052039ef9d
Samuel P Leo <samuelpleo79@gmail.com>
2025-05-09 10:50:09 +0530

    Week 1 Backend

--- Backend/auth.py
+++ Backend/auth.py
@@ -8,6 +8,13 @@ router = APIRouter()
 
 @router.post("/register",response_model=schemas.UserOut)
 def register(user: schemas.UserCreate, db: Session = Depends(get_db)):
+  '''
+  Function to register a new user into the database
+  
+  existing_user: stores a boolean value if a user is present in the database or not.
+  hashed: stores the hashed password.
+  new_user: stores all the details of the user.
+  '''
   existing_user = db.query(models.User).filter(models.User.email == user.email).first()
   if existing_user:
     raise HTTPException(status_code=400,detail="Email already exists")
@@ -20,6 +27,12 @@ def register(user: schemas.UserCreate, db: Session = Depends(get_db)):
 
 @router.post("/login")
 def login(user: schemas.UserLogin, db: Session = Depends(get_db)):
+  '''
+  Function gives a token to the use after a successful login.
+
+  db_user: stores a boolean value if the user is present in the database or not.
+  token: stores the token for using the app.
+  '''
   db_user = db.query(models.User).filter(models.User.email == user.email).first()
   if not db_user:
     raise HTTPException(status_code=400,detail="Wrong email or Password")
--- Backend/models.py
+++ Backend/models.py
@@ -1,11 +1,29 @@
 from sqlalchemy import Column, Integer, String
 from database import Base
 
-# Creating the table named 'users'
-class User(Base):
-  __tablename__ = "users"
 
+class User(Base):
+  '''
+  Table for storing user details in the database
+
+  id: store the id of a user which can be later used for accessing a user.
+  username: stores the name of the user.
+  email: stores the user's email.
+  hashed_password: it stores the password of the user after hashing it.
+  role: stores the role assigned to a user.
+  '''
+  __tablename__ = "users"
   id = Column(Integer, primary_key=True,index=True,autoincrement=True)
   username =  Column(String,nullable=False)
   email  = Column(String,unique=True,nullable=False,index=True,)
   hashed_password = Column(String,nullable=False)
+  '''
+  role = Column(String,default="user")
+  
+
+
+# Creating table for storing Projects
+class Project(Base):
+  __tablename__= "projects"
+
+  '''
